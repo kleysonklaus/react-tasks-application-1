ff5cce1b1f59a85f081a20e4b3d0901b49fda448
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-29 19:19:55 -0500

    feat(taskList): show all task list with useState and useEffect

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,11 +1,31 @@
-import { tasks } from "./task";
+import { tasks as data } from "./task";
+import { useState, useEffect } from 'react'
 
-console.log(tasks)
+console.log(data)
 
 function TaskList() {
-    return (
 
-        <h1>Hola</h1>
+    const [tasks, setTasks] = useState([])
+
+    useEffect((e) => {
+        setTasks(data)
+    }, [])
+
+    if (tasks.length === 0) {
+        return <h1>No hay tareas aun ... obteniendo!</h1>
+    }
+
+    return (
+        <div>
+            {
+                tasks.map((element, index) => (
+                    <div key={element.id}>
+                        <h1>{element.title}</h1>
+                    </div>
+                )
+                )
+            }
+        </div>
     )
 }
 
