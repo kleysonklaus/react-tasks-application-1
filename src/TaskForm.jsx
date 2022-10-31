3d459bed6d28c67ac1c2ea572201fca8acf306c9
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-30 23:29:31 -0500

    feat(taskForm): with useState

--- src/TaskForm.jsx
+++ src/TaskForm.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react'
+
 function TaskForm() {
+    const [title, useTitle] = useState('')
     return (
         <form>
-            <input placeholder="Escribe tu tarea" />
+            <input placeholder="Escribe tu tarea" onChange={(e) => {
+                useTitle(e.target.value)
+            }} />
             <button>Guardar</button>
         </form>
     )
