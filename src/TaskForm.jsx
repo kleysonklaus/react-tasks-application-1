45832fd6517234d69c046999149358bd03e99838
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-30 23:31:15 -0500

    feat(taskForm): prevent default to not refresh page

--- src/TaskForm.jsx
+++ src/TaskForm.jsx
@@ -2,8 +2,14 @@ import { useState } from 'react'
 
 function TaskForm() {
     const [title, useTitle] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(title)
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input placeholder="Escribe tu tarea" onChange={(e) => {
                 useTitle(e.target.value)
             }} />
