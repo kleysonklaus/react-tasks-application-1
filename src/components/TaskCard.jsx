bc007010e52b810ca16d4d250420b6107ec8ca63
kleyson klaus <kleyson.zur44@gmail.com>
2022-10-31 23:43:01 -0500

    feat(taskCard): button to delete card,. not functionality yet

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 function TaskCard({ task }) {
+
+    function mostrarAlerta() {
+        alert(task.id)
+    }
+
     return (
         <div key={task.id}>
             <h1>{task.title}</h1>
             <p>{task.description}</p>
+            <button onClick={mostrarAlerta} >Eliminar Tarea</button>
         </div>
     )
 }
