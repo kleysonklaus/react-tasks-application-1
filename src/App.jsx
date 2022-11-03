6539ace7378f6a79201f155f4057a5956fb08f70
kleyson klaus <kleyson.zur44@gmail.com>
2022-11-02 23:21:22 -0500

    feat(App): clear code of data, now is only a simple component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
 import TaskForm from './components/TaskForm'
 import TaskList from './components/TaskList'
-import { tasks as data } from "./data/task";
-
-import { useState, useEffect } from 'react';
 
 function App() {
 
-  const [tasks, setTasks] = useState([])
-
-  useEffect((e) => {
-    setTasks(data)
-  }, [])
-
-  function createTask(taskTitle, taskDescription) {
-    const _newData = {
-      id: tasks.length,
-      title: taskTitle,
-      description: taskDescription
-    }
-    setTasks([...tasks, _newData])
-  }
-
-  function deleteTask(taskId) {
-    setTasks(tasks.filter(task => task.id !== taskId))
-  }
-
   return (
     <>
-      <TaskForm createTask={createTask} />
-      <TaskList tasks={tasks} deleteTask={deleteTask} />
+      <TaskForm />
+      <TaskList />
     </>
   )
 }
