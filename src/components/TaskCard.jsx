491738679d0f4ac4fdaa1694b04622c46751b2e5
kleyson klaus <kleyson.zur44@gmail.com>
2022-11-02 22:46:33 -0500

    feat(context): expample to use

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import TaskContext from "../context/TaskContext";
 
 function TaskCard({ task, deleteTask }) {
 
     return (
-        <div key={task.id}>
-            <h1>{task.title}</h1>
-            <p>{task.description}</p>
-            <button onClick={() => deleteTask(task.id)} >Eliminar Tarea</button>
-        </div>
+        <TaskContext>
+            <div key={task.id}>
+                <h1>{task.title}</h1>
+                <p>{task.description}</p>
+                <button onClick={() => deleteTask(task.id)} >Eliminar Tarea</button>
+            </div>
+        </TaskContext>
     )
 }
 
