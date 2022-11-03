417eee1355e05b95f67472f05f05fccdd7fad92d
kleyson klaus <kleyson.zur44@gmail.com>
2022-11-02 23:21:58 -0500

    feat(TaskContext): now all data and functions are in TaskContext

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,12 +1,35 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
+import { tasks as data } from "../data/task";
 
 export const TaskContext = createContext()
 
 export function TaskcontextProvider(props) {
-    let x = 20
+    const [tasks, setTasks] = useState([])
+
+    function createTask(taskTitle, taskDescription) {
+        const _newData = {
+            id: tasks.length,
+            title: taskTitle,
+            description: taskDescription
+        }
+        setTasks([...tasks, _newData])
+    }
+
+
+    function deleteTask(taskId) {
+        setTasks(tasks.filter(task => task.id !== taskId))
+    }
+
+    useEffect((e) => {
+        setTasks(data)
+    }, [])
 
     return (
-        <TaskContext.Provider value={x}>
+        <TaskContext.Provider value={{
+            tasks: tasks,
+            deleteTask: deleteTask,
+            createTask: createTask
+        }}>
             {props.children}
         </TaskContext.Provider>
     )
