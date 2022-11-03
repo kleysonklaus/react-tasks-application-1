3fc4e97178585f2f382f373a0ac48e2cb255bd08
kleyson klaus <kleyson.zur44@gmail.com>
2022-11-02 22:47:11 -0500

    feat(TaskCard): remove basic context

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,15 +1,12 @@
-import TaskContext from "../context/TaskContext";
 
 function TaskCard({ task, deleteTask }) {
 
     return (
-        <TaskContext>
-            <div key={task.id}>
-                <h1>{task.title}</h1>
-                <p>{task.description}</p>
-                <button onClick={() => deleteTask(task.id)} >Eliminar Tarea</button>
-            </div>
-        </TaskContext>
+        <div key={task.id}>
+            <h1>{task.title}</h1>
+            <p>{task.description}</p>
+            <button onClick={() => deleteTask(task.id)} >Eliminar Tarea</button>
+        </div>
     )
 }
 
