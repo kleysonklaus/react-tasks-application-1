1a17072290dda657e785f8f891c0ffba580087a8
kleyson klaus <kleyson.zur44@gmail.com>
2022-11-02 22:55:16 -0500

    feat(main): implement use provider context

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { TaskcontextProvider } from "./context/TaskContext";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TaskcontextProvider>
+      <App />
+    </TaskcontextProvider>
   </React.StrictMode>
 )
